3cc93ac3eeb013e7a3063d7f75f60d86cfb35ca2
Matija <siouxx0612@gmail.com>
2022-04-25 15:04:18 +0200

    Add comments

--- index.js
+++ index.js
@@ -9,28 +9,35 @@ canvas.height = window.innerHeight
 //create graviti to accelerate velocity on axis y
 const gravity = 0.5
 class Player {
-
+// our Player starting position
     constructor() {
         this.position = {
             x: 100,
             y: 100
         }
+        //Players velocity
         this.velocity = {
             x: 0,
-            y: 0
+            y: 1
         }
+        //Our Players size
         this.width = 30
         this.height = 50
     }
+
+    //Method to define our Player
     draw() {
+
         c.fillStyle = 'red'
+        //Where is our Player and how big it is
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
-
+    
+//Update our players property over time
     update() {
         this.draw()
         this.position.y += this.velocity.y
-
+// check velocity, if we are above the bottom then stop velocity ( this.velocity.y = 0 )
         if (this.position.y + this.height + this.velocity.y <= canvas.height)
             this.velocity.y += gravity
         else this.velocity.y = 0
