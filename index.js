65aeb990b5e1d4a4efbbfecf17f55ce7c8b656ef
Matija <siouxx0612@gmail.com>
2022-04-28 09:23:38 +0200

    Add scrolling the background, change our Platform so it can be created multiple platforms and add them into conditional to recognise

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const c = canvas.getContext('2d');
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-//create graviti to accelerate velocity on axis y
+//create gravity to accelerate velocity on axis-y
 const gravity = 0.5
 
 
@@ -18,16 +18,15 @@ class Player {
         //Players velocity
         this.velocity = {
             x: 0,
-            y: 1
+            y: 5
         }
         //Our Players size
         this.width = 30
         this.height = 50
     }
 
-    //Method to define our Player
+    //Method to define our Player and create our object size
     draw() {
-
         c.fillStyle = 'red'
         //Where is our Player and how big it is
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
@@ -54,17 +53,18 @@ class Player {
 }
 
 class Platform {
-    constructor() {
+    constructor({x, y}) {
         this.position = {
-            x: 200,
-            y: 400
+            x,
+            //Its the same
+            y: y
         }
         this.width = 200
         this.height = 20
     }
 
     draw() {
-        //draws rectangle like we did with our Player
+        //draws rectangle like we did with our Player. Create our platform size
         c.fillStyle = 'blue'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
 
@@ -72,13 +72,15 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+// const platform = new Platform()
+//create multiple platforms, and create one platfor in that array
+const platforms = [new Platform({x: 200, y: 200}), new Platform({x: 400, y: 400})]
 
 
 const keys = {
     right: {
         pressed: false
-    }, 
+    },
     left: {
         pressed: false
     }
@@ -91,25 +93,41 @@ function animate() {
     //get our context and maintain shape of the rectangle. Its gonna clear our canvas, take everything off it.
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
-    platform.draw()
+    //loop trough array and select one individual platform in that array. platform, arbitrarily named platform but it makes sence 
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+
 
     //Check if the keys are true or false for movement Left or Right
-    if(keys.right.pressed){
+    if (keys.right.pressed && player.position.x < 600) {
         player.velocity.x = 5
-    } else if (keys.left.pressed) {
+    } else if (keys.left.pressed && player.position.x > 100) {
         player.velocity.x = -5
-    } else  player.velocity.x = 0
-
+    } else {
+        player.velocity.x = 0
+        if (keys.right.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        }
+        else if (keys.left.pressed) {
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
+    }
     //rectangular coalision detection
     //check if our players y position plus height, so the bottom of the player whether or not the bottom of the player
     //is less than the top of our platform
-    if(player.position.y + player.height <= platform.position.y && 
-        player.position.y + player.height + player.velocity.y >= platform.position.y &&
-        player.position.x + player.width >= platform.position.x &&
-        player.position.x <= platform.position.x + platform.width) {
-        player.velocity.y = 0
-    }
-    
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
 }
 animate()
 //EventListener for our keys on pres down for Player movment
