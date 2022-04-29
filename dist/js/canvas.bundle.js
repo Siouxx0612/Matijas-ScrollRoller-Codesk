6b8e22c26009003df06b1561d4825f7f702c98e4
Matija <siouxx0612@gmail.com>
2022-04-29 09:23:04 +0200

    Adding background and hills, function for dynamic image and paralax scroll

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -86,6 +86,32 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/img/background.png":
+/*!********************************!*\
+  !*** ./src/img/background.png ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "072d51bcc9c09311d4c2a6708b05bddc.png");
+
+/***/ }),
+
+/***/ "./src/img/hills.png":
+/*!***************************!*\
+  !*** ./src/img/hills.png ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = (__webpack_require__.p + "cfffe4c371f5e11d372b398a87c51dd0.png");
+
+/***/ }),
+
 /***/ "./src/img/platform.png":
 /*!******************************!*\
   !*** ./src/img/platform.png ***!
@@ -109,6 +135,8 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _img_platform_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../img/platform.png */ "./src/img/platform.png");
+/* harmony import */ var _img_background_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../img/background.png */ "./src/img/background.png");
+/* harmony import */ var _img_hills_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../img/hills.png */ "./src/img/hills.png");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -116,11 +144,13 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
 
+
+
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d'); // canvas.width = window.innerWidth
 // canvas.height = window.innerHeight
 
-canvas.width = 1024;
+canvas.width = 1400;
 canvas.height = 576; //create gravity to accelerate velocity on axis-y
 
 var gravity = 0.5;
@@ -207,19 +237,61 @@ var Platform = /*#__PURE__*/function () {
   return Platform;
 }();
 
-var image = new Image();
-image.src = _img_platform_png__WEBPACK_IMPORTED_MODULE_0__["default"];
+var BackgroundObject = /*#__PURE__*/function () {
+  function BackgroundObject(_ref2) {
+    var x = _ref2.x,
+        y = _ref2.y,
+        image = _ref2.image;
+
+    _classCallCheck(this, BackgroundObject);
+
+    this.position = {
+      x: x,
+      //Its the same
+      y: y
+    };
+    this.image = image;
+    this.width = image.width;
+    this.height = image.height;
+  }
+
+  _createClass(BackgroundObject, [{
+    key: "draw",
+    value: function draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+
+  return BackgroundObject;
+}();
+
+function createImage(imageSrc) {
+  var image = new Image();
+  image.src = imageSrc;
+  return image;
+}
+
+var platformImage = createImage(_img_platform_png__WEBPACK_IMPORTED_MODULE_0__["default"]);
 var player = new Player(); // const platform = new Platform()
 //create multiple platforms, and create one platfor in that array
 
 var platforms = [new Platform({
   x: -1,
   y: 470,
-  image: image
+  image: platformImage
 }), new Platform({
-  x: image.width - 3,
+  x: platformImage.width - 3,
   y: 470,
-  image: image
+  image: platformImage
+})];
+var backgroundObjects = [new BackgroundObject({
+  x: -1,
+  y: -1,
+  image: createImage(_img_background_png__WEBPACK_IMPORTED_MODULE_1__["default"])
+}), new BackgroundObject({
+  x: -1,
+  y: -1,
+  image: createImage(_img_hills_png__WEBPACK_IMPORTED_MODULE_2__["default"])
 })];
 var keys = {
   right: {
@@ -237,7 +309,10 @@ function animate() {
   // c.clearRect(0, 0, canvas.width, canvas.height)
 
   c.fillStyle = 'white';
-  c.fillRect(0, 0, canvas.width, canvas.height); //loop trough array and select one individual platform in that array. platform, arbitrarily named platform but it makes sence 
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  backgroundObjects.forEach(function (backgroundObject) {
+    backgroundObject.draw();
+  }); //loop trough array and select one individual platform in that array. platform, arbitrarily named platform but it makes sence 
 
   platforms.forEach(function (platform) {
     platform.draw();
@@ -256,10 +331,16 @@ function animate() {
       platforms.forEach(function (platform) {
         platform.position.x -= 5;
       });
+      backgroundObjects.forEach(function (backgroundObject) {
+        backgroundObject.position.x -= 2;
+      });
     } else if (keys.left.pressed) {
       howFarScrollOffset -= 5;
       platforms.forEach(function (platform) {
         platform.position.x += 5;
+      });
+      backgroundObjects.forEach(function (backgroundObject) {
+        backgroundObject.position.x += 2;
       });
     }
   } // console.log(howFarScrollOffset)
@@ -273,16 +354,17 @@ function animate() {
       player.velocity.y = 0;
     }
   });
+  var winMsg = document.querySelector("h1");
 
-  if (howFarScrollOffset > 2000) {
-    console.log('YOU WIN');
+  if (howFarScrollOffset > 1000) {
+    winMsg.innerText = "YOU WON";
   }
 }
 
 animate(); //EventListener for our keys on pres down for Player movment
 
-window.addEventListener('keydown', function (_ref2) {
-  var keyCode = _ref2.keyCode;
+window.addEventListener('keydown', function (_ref3) {
+  var keyCode = _ref3.keyCode;
 
   switch (keyCode) {
     case 65:
@@ -310,8 +392,8 @@ window.addEventListener('keydown', function (_ref2) {
       break;
   }
 });
-window.addEventListener('keyup', function (_ref3) {
-  var keyCode = _ref3.keyCode;
+window.addEventListener('keyup', function (_ref4) {
+  var keyCode = _ref4.keyCode;
 
   switch (keyCode) {
     case 65:
